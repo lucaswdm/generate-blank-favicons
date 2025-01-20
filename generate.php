89a2ccae55ddc28f2e3714b04095c1ab3a5680f1
lucaswdm <lucas@data2.com.br>
2025-01-20 20:49:40 -0300

    Update generate.php

--- generate.php
+++ generate.php
@@ -9,5 +9,17 @@ $DIRS = array_merge(
 
 foreach($DIRS as $DIR)
 {
+    $EXP = explode($DIR, '/');
+    print_r($EXP);
     echo $DIR . PHP_EOL;
+}
+
+
+function validateDomain($domain_name)
+{
+    if(filter_var($domain_name, FILTER_VALIDATE_IP)) return false;
+    if(strpos($domain_name, '.') === false) return false;
+    return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) //valid chars check
+            && preg_match("/^.{1,253}$/", $domain_name) //overall length check
+            && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)   ); //length of each label
 }
